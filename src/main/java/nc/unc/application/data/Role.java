8e6e9a3e62cdfe14de17356761cfcf40f42191d2
start.vaadin.com <no-reply@vaadin.com>
2022-03-16 03:38:37 +0000

    Generated project

---
+++ src/main/java/nc/unc/application/data/Role.java
@@ -0,0 +1,5 @@
+package nc.unc.application.data;
+
+public enum Role {
+    USER, ADMIN;
+}
---
+++ src/main/java/nc/unc/application/data/service/UserRepository.java
@@ -0,0 +1,10 @@
+package nc.unc.application.data.service;
+
+import java.util.UUID;
+import nc.unc.application.data.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    User findByUsername(String username);
+}

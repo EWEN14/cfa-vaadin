f167f8cccf4412bc02e9a7ce56905ba34a1cbed1
lika <tagatalelika@gmail.com>
2022-04-09 14:07:28 +1100

    Développement(Back-Formation)

--- src/main/java/nc/unc/application/data/service/FormationService.java
+++ src/main/java/nc/unc/application/data/service/FormationService.java
@@ -1,2 +1,48 @@
-package nc.unc.application.data.service;public class FormationService {
+package nc.unc.application.data.service;
+
+import nc.unc.application.data.entity.Formation;
+import nc.unc.application.data.repository.FormationRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FormationService {
+
+
+  private FormationRepository formationRepository;
+
+  //Constructeur
+  public FormationService(FormationRepository formationRepository) {
+    this.formationRepository = formationRepository;
+  }
+
+  //Récupérer tous les formations ou la(les) formation(s) si filter n'est pas null
+  public List<Formation> findAllFormations(String filter){
+
+    if(filter == null || filter.isEmpty()){
+      return formationRepository.findAll();
+    } else {
+      return formationRepository.search(filter);
+    }
+  }
+
+  //Supprimer une formation
+  public void removeFormation(Formation formation){
+    formationRepository.delete(formation);
+  }
+
+  //Sauvegarder une formation
+  public void addFormation(Formation formation){
+    if (formation == null) {
+      System.err.println("La formation est nul, le formulaire est-il bien connecté à l'application ?");
+      return;
+    }
+    formationRepository.save(formation);
+  }
+
+  //Retourne le nombre de formations
+  public void countFormations(){
+    formationRepository.count();
+  }
 }
